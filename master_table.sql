3930c3b23203b6ea1d37420c59335c798854c0b7
raditya budi handoko <radityabh@gmail.com>
2017-06-13 13:50:04 +0700

    Add files via upload

--- master_table.sql
+++ master_table.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 08, 2017 at 09:14 AM
+-- Generation Time: Jun 13, 2017 at 08:48 AM
 -- Server version: 10.1.10-MariaDB
 -- PHP Version: 5.6.15
 
@@ -23,11 +23,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` smallint(6) NOT NULL,
+  `nama` tinytext NOT NULL,
+  `merk` tinytext NOT NULL,
+  `type/seri` tinytext NOT NULL,
+  `satuan` varchar(15) NOT NULL,
+  `masa_pakai` char(2) NOT NULL,
+  `stok` mediumint(9) NOT NULL,
+  `harga` tinytext NOT NULL,
+  `ket` tinytext NOT NULL,
+  `del` tinyint(1) NOT NULL,
+  `del_on` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `mod_by` tinytext NOT NULL,
+  `mod_on` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `log`
 --
 
 CREATE TABLE `log` (
   `id` bigint(20) NOT NULL,
+  `id_user` bigint(20) NOT NULL,
   `even` text NOT NULL,
   `user_id` tinytext NOT NULL,
   `time` timestamp NULL DEFAULT '0000-00-00 00:00:00',
@@ -37,16 +60,24 @@ CREATE TABLE `log` (
   `modified_on` timestamp NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `log`
+-- Table structure for table `transaksi`
 --
 
-INSERT INTO `log` (`id`, `even`, `user_id`, `time`, `del`, `del_on`, `modified_by`, `modified_on`) VALUES
-(1, 'insert User dengan id 1', '1234565', '2017-06-08 03:58:31', 0, '0000-00-00 00:00:00', '', '0000-00-00 00:00:00'),
-(2, 'insert User dengan id 2', '1234565', '2017-06-08 03:59:19', 0, '0000-00-00 00:00:00', '', '0000-00-00 00:00:00'),
-(3, 'insert User dengan id 1', '1234565', '2017-06-08 04:01:18', 0, '0000-00-00 00:00:00', '', '0000-00-00 00:00:00'),
-(4, 'Hapus User dengan id 4', '1234565', '2017-06-08 04:27:37', 0, '0000-00-00 00:00:00', '', '0000-00-00 00:00:00'),
-(5, 'insert User dengan id 25', '1234565', '2017-06-08 04:38:54', 0, '0000-00-00 00:00:00', '', '0000-00-00 00:00:00');
+CREATE TABLE `transaksi` (
+  `id` bigint(20) NOT NULL,
+  `waktu_trx` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `barang_id` smallint(6) NOT NULL,
+  `qty` varchar(4) NOT NULL,
+  `harga` varchar(9) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `del` tinyint(1) NOT NULL,
+  `del_on` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `mod_by` varchar(10) NOT NULL,
+  `mod_on` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -55,7 +86,7 @@ INSERT INTO `log` (`id`, `even`, `user_id`, `time`, `del`, `del_on`, `modified_b
 --
 
 CREATE TABLE `user` (
-  `id` bigint(4) NOT NULL,
+  `id` bigint(20) NOT NULL,
   `nama` tinytext NOT NULL,
   `pass` tinytext NOT NULL,
   `email` tinytext NOT NULL,
@@ -66,18 +97,14 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`id`, `nama`, `pass`, `email`, `del`, `del_on`, `modified_by`, `modified_on`) VALUES
-(1, 'qwre', 'qwre', 'qwre', 0, '0000-00-00 00:00:00', '', '0000-00-00 00:00:00'),
-(2, 'qwre', 'qwre', 'qwre', 0, '0000-00-00 00:00:00', '', '0000-00-00 00:00:00'),
-(4, 'qwre', 'qwre', 'qwre', 1, '2017-06-08 04:27:37', '1234565', '2017-06-08 04:27:37'),
-(25, 'qwre', 'qwre', 'qwre', 0, '0000-00-00 00:00:00', '', '0000-00-00 00:00:00');
-
---
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `log`
